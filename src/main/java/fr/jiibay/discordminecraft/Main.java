bf043bc30c57a0928a56c1fad2a37dfe625370a3
BuildTools <jeanbaptiste07262004.colin@gmail.com>
2020-06-20 22:41:49 +0200

    appDiscordMinecraft

---
+++ src/main/java/fr/jiibay/discordminecraft/Main.java
@@ -0,0 +1,22 @@
+package fr.jiibay.discordminecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+
+    }
+
+    @Override
+    public void onEnable() {
+        System.out.println("test");
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
---
+++ src/main/java/fr/jiibay/discordminecraft/Managers/JdaRegister.java
@@ -0,0 +1,6 @@
+package fr.jiibay.discordminecraft.Managers;
+
+public class JdaRegister {
+
+
+}
